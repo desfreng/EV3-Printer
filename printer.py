8b9a021e951cd003f8156f73e8094f12fd397f4d
Gabriel Desfrene <28400971+desfreng@users.noreply.github.com>
2020-04-15 15:16:57 +0200

    Add Documentations

--- printer.py
+++ printer.py
@@ -1,25 +1,26 @@
 # coding=utf-8
 
-"""The Printer module is a set of classes witch allow interacting at a low level with the printer."""
+"""The Printer module is a set of classes witch allow interacting at a low level with the printer.
+"""
 
 from time import sleep, time
 
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D
 from ev3dev2.sensor.lego import ColorSensor
 
-debug = True
-_time = time()
+DEBUG = True
+_TIME = time()
 
 
-def _debug(obj, msg, begin_time=_time):
-    if debug:
+def _debug(obj, msg, begin_time=_TIME):
+    if DEBUG:
         print("[{}]({}) {}".format(time() - begin_time, obj.__class__.__name__, msg))
 
 
 class Pen:
-    default_power = 20
+    """Pen class witch allow managing the pen position. """
 
-    def __init__(self, power=default_power):
+    def __init__(self, power=20):
         _debug(self, "Creating Pen instance")
         self.default_power = power
 
@@ -30,19 +31,25 @@ class Pen:
         self._m.stop_action = LargeMotor.STOP_ACTION_HOLD
         self.reset()
 
-    def up(self, power=default_power):
+    def up(self, power=None):
         """ Set pen to 'up' position (only if is not already)
 
         :param power: Power of the rotation
         """
+        if power is None:
+            power = self.default_power
+
         if not self.is_up:
             self._m.on_to_position(power, self._pen_up_position)
 
-    def down(self, power=default_power):
+    def down(self, power=None):
         """ Set pen to 'down' position (only if is not already)
 
         :param power: Power of the rotation
         """
+        if power is None:
+            power = self.default_power
+
         if self.is_up:
             self._m.on_to_position(power, self._pen_down_position)
 
@@ -65,11 +72,14 @@ class Pen:
         else:
             self.up()
 
-    def reset(self, power=default_power):
+    def reset(self, power=None):
         """Reset the pen position (set it to 'up')
 
         :param power: Power used to move the pen
         """
+        if power is None:
+            power = self.default_power
+
         self._m.on(-power)
         self._m.wait_until_not_moving()
         self._m.off()
@@ -79,7 +89,8 @@ class Pen:
     def setup(self, validator):
         """ Setup the pen, define up and down position.
 
-        """
+        :param validator: A callable objet used to validate the pen down position.
+        When `True` is returned, the pen position will be saved to the down position."""
         self.reset()
         self._pen_up_position = self._m.position
         self._m.off(False)
@@ -91,24 +102,29 @@ class Pen:
         self.up()
 
     def save_energy(self):
+        """Save energy and release motor's holding state"""
         self._m.off(False)
 
 
 class Carriage:
-    default_power = 30
+    """Carriage class witch allow managing the position. """
 
-    def __init__(self, power=default_power):
+    def __init__(self, power=30):
+        _debug(self, "Creating Carriage instance")
+
         self.default_power = power
         self._delta = 0
 
         self._m = LargeMotor(OUTPUT_B)
         self.reset()
 
-    def reset(self, power=default_power):
+    def reset(self, power=None):
         """Reset the carriage position and define a new origin for carriage's position
 
         :param power: Power used to move the carriage
         """
+        if power is None:
+            power = self.default_power
 
         self.right_limit(power=power, soft_limit=False)
         self._m.on_for_degrees(power, 50)
@@ -116,18 +132,24 @@ class Carriage:
 
         self.go_to(0, power)
 
-    def left(self, power=default_power):
+    def left(self, power=None):
         """Move carriage (pen will move too) to the 'left'
 
         :param power: Power of the rotation
         """
+        if power is None:
+            power = self.default_power
+
         self._m.on(power)
 
-    def right(self, power=default_power):
+    def right(self, power=None):
         """Move carriage (pen will move too) to the 'right'
 
         :param power: Power of the rotation
         """
+        if power is None:
+            power = self.default_power
+
         self._m.on(-power)
 
     def stop(self):
@@ -148,24 +170,50 @@ class Carriage:
     def position(self, value):
         """ Set carriage position
 
-        :param value: New carriage position"""
+        :param value: Reached position"""
         self.go_to(value)
 
-    def go_to(self, position, power=default_power, override=False):
+    def go_to(self, position, power=None, override=False):
+        """Move carriage to absolute position `position`
+
+        :param position: Position reached
+        :param power: Power used to move
+        :param override: if true, bypass limits"""
+
+        if power is None:
+            power = self.default_power
+
         _debug(self, "Reached position is {}".format(position))
 
-        if (not override) and (not -50 < position < 1240):
+        if (not override) and (not -50 <= position <= 1240):
             raise ValueError("Position is out of reachable bounds.")
 
         self._m.on_to_position(power, position + self._delta)
 
-    def move(self, position, power=default_power, override=False):
-        self.go_to(self.position + position, power, override)
+    def move(self, value, power=None, override=False):
+        """Move carriage of `value` degrees
+
+        :param value: Position reached
+        :param power: Power used to move
+        :param override: if true, bypass limits"""
+        if power is None:
+            power = self.default_power
+
+        self.go_to(self.position + value, power, override)
 
     def save_energy(self):
+        """Save energy and release motor's holding state"""
         self._m.off(False)
 
-    def right_limit(self, soft_limit=True, power=default_power):
+    def right_limit(self, soft_limit=True, power=None):
+        """Go to the right limit
+
+        :param soft_limit: If True, carriage will move backward to avoid motor forcing
+        :param power: power used to move"""
+
+        if power is None:
+            power = self.default_power
+
         self.right(power)
 
         self._m.wait_until_not_moving()
@@ -173,7 +221,14 @@ class Carriage:
         if soft_limit:
             self.move(50, power)
 
-    def left_limit(self, soft_limit=True, power=default_power):
+    def left_limit(self, soft_limit=True, power=None):
+        """Go to the left limit
+
+        :param soft_limit: If True, carriage will move backward to avoid motor forcing
+        :param power: power used to move"""
+        if power is None:
+            power = self.default_power
+
         self.left(power)
 
         self._m.wait_until_not_moving()
@@ -184,8 +239,11 @@ class Carriage:
 
 
 class Rollers:
+    """Rollers class witch allow managing paper. """
 
     def __init__(self, power=30, prevent_paper_blocking=False):
+        _debug(self, "Creating Rollers instance")
+
         self.default_power = power
         self._delta_in = 0
         self._delta_out = 0
@@ -203,7 +261,7 @@ class Rollers:
         self.reset(prevent_paper_blocking)
 
     def reset(self, prevent_paper_blocking=False, power=None):
-        """Reset the rollers position and define a new origin for rollers position
+        """Eject paper if present and prevent paper blocking
 
         :param prevent_paper_blocking: if true, roller will move to avoid paper blocking
         :param power: Power used to move rollers
@@ -240,25 +298,29 @@ class Rollers:
         self._out.on(-power)
 
     def stop(self):
-        """ Stop moving carriage
+        """ Stop moving rollers
 
         """
         self._in.off()
         self._out.off()
 
     def save_energy(self):
+        """Save energy and release motor's holding state"""
         self._in.off(False)
         self._out.off(False)
 
     @property
     def has_paper(self):
+        """ Get the paper state
+
+        :return True if paper present"""
         return self._paper_taken
 
     @property
     def position(self):
-        """ Get carriage position
+        """ Get rollers position
 
-        :return: carriage position
+        :return: rollers position
         """
         if not self.has_paper:
             return None
@@ -267,12 +329,17 @@ class Rollers:
 
     @position.setter
     def position(self, value):
-        """ Set carriage position
+        """ Set Rollers position
 
-        :param value: New roller position"""
+        :param value: position reached"""
         self.go_to(value)
 
     def go_to(self, position, power=None, override=False):
+        """Go to absolute position `position`
+
+        :param position: Position Reached
+        :param power: Power used to move
+        :param override: if true, bypass limits"""
         if not self.has_paper:
             raise ValueError("There is no paper.")
 
@@ -293,27 +360,35 @@ class Rollers:
         _debug(self, "Target In {}".format(target_in))
         _debug(self, "Target Out {}".format(target_out))
 
-        if (not override) and (not 0 < position < 515):
+        if (not override) and (not 0 <= position <= 515):
             raise ValueError("Position is out of reachable bounds.")
 
         self._in.on_to_position(power, target_in, block=False)
         self._out.on_to_position(power, target_out, block=True)
 
-    def move(self, position, power=None):
+    def move(self, value, power=None):
+        """Move rollers (and paper if present) of `value` degrees
+
+        :param value: Degrees to move
+        :param power; Power used to move"""
         if power is None:
             power = self.default_power
 
-        target_in = self._in.position + position
-        target_out = self._out.position + position
-
-        self._in.on_to_position(power, target_in, block=False)
-        self._out.on_to_position(power, target_out, block=True)
+        self._in.on_to_position(power, self._in.position + value, block=False)
+        self._out.on_to_position(power, self._out.position + value, block=True)
 
     def up_limit(self, power=None):
+        """Go to paper up limit
+
+        :param power: Power used to move"""
+
         self.go_to(0, power)
 
     def down_limit(self, power=None):
-        self.go_to(0, power)
+        """Go to paper down limit
+
+        :param power: Power used to move"""
+        self.go_to(515, power)
 
     def take_paper(self, power=None, power_grip=15):
         """ Take paper into printer and stretch it
@@ -336,7 +411,7 @@ class Rollers:
         self._delta_out = self._out.position
 
     def eject_paper(self, power=None):
-        """ Eject from the printer
+        """ Eject paper
 
         :param power: Power used to eject paper
         """
@@ -356,19 +431,10 @@ class Rollers:
         self._delta_in = 0
         self._delta_out = 0
 
-    def __repr__(self):
-        return "\tRollers\n" \
-               "\t In \t Out\n" \
-               "Dlt \t {} \t {}\n" \
-               "Abs \t {} \t {}\n" \
-               "Rel \t {} \t {}\n".format(self._delta_in, self._delta_out, self._in.position, self._out.position,
-                                          (self.position[0] if self.has_paper else "None"),
-                                          (self.position[1] if self.has_paper else "None"))
-
 
 class Printer:
-    """A Basic Printer class to interact with the printer at low level. Use this only if you need to interact exactly
-    with the printer. """
+    """A Basic Printer class to interact with the printer at low level. Use this only if you need
+    to interact exactly with the printer. """
     #
     # def __init__(self, prevent_paper_blocking=True):
     #     """ Constructor of Printer class
